2746c156480015c69bfe82d50179bed18409ec13
dekofar-dev <dekofardeveloper@gmail.com>
2026-01-10 00:03:49 +0300

    adasdasdasd

---
+++ Dekofar.HyperConnect.Infrastructure/Migrations/20260109210233_AddIsActiveToShopifyStores.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dekofar.HyperConnect.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsActiveToShopifyStores : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "InstalledAt",
+                table: "ShopifyStores",
+                newName: "InstalledAtUtc");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "InstalledAtUtc",
+                table: "ShopifyStores",
+                newName: "InstalledAt");
+        }
+    }
+}
